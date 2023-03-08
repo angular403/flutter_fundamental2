c54d5453b2c0f535a05d046cd22f87d5fae25bd3
Andrew Napitupulu <angularsprings403@gmail.com>
2023-03-08 14:07:14 +0700

    multi language flutter

---
+++ lib/bloc/user_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_fundamental/user.dart';
+
+part 'user_event.dart';
+part 'user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  UserBloc() : super(UserInitial()) {
+    @override
+    Stream<UserState> mapEventToState(
+      UserEvent event,
+    ) async* {
+      if (event is ChangeNameEvent) {
+        yield UserLoaded(User(name: event.name, age: state.user.age));
+      } else if (event is BirthdayEvent) {
+      yield UserLoaded(User(name: state.user.name, age: state.user.age + 1));
+      }
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_fundamental/bloc/user_bloc.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: [
+        Locale('id'),
+        Locale('en', 'US'),
+      ],
+      path: 'assets/translations',
+      startLocale: Locale('id'),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,48 +24,118 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Latihan(),
+    return BlocProvider(
+      create: (context) => UserBloc(),
+      child: MaterialApp(
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        debugShowCheckedModeBanner: false,
+        home: MultiLanguage(),
+      ),
     );
   }
 }
 
-class Latihan extends StatelessWidget {
-  const Latihan({Key? key}) : super(key: key);
+class MultiLanguage extends StatelessWidget {
+  const MultiLanguage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Latihan Elevated Button'),
+        title: Text('Multi Language App'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('greeting').tr(),
+              const SizedBox(height: 10),
+              const Text('name')
+                  .tr(args: ['Andrew Wiliam Napitupulu', 'Indonesia']),
+              const SizedBox(height: 10),
+              const Text('film')
+                  .tr(namedArgs: {'title': 'My Name', 'platform': 'Netflix'}),
+              const SizedBox(height: 10),
+              const Text('character').tr(gender: 'female'),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      context.setLocale(Locale('id'));
+                    },
+                    child: Text('Indonesia'),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.setLocale('en_US'.toLocale());
+                    },
+                    child: Text('US English'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: BlocBuilder<UserBloc, UserState>(
+          builder: (context, state) {
+            return Text('Welcome ${state.user.name}');
+          },
+        ),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Button 1 '),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.red,
-                  minimumSize: Size(200, 40),
-                  shape: StadiumBorder(),
-                  elevation: 2,
-                  shadowColor: Colors.green,
-                  onSurface: Colors.red,
-                  side: BorderSide(width: 2, color: Colors.red.shade900)),
+            BlocBuilder<UserBloc, UserState>(
+              builder: (context, state) {
+                return Text(
+                    'Hi.. My Name ${state.user.name} and my age is  ${state.user.age} year');
+              },
             ),
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: Icon(Icons.ac_unit),
-              label: Text('Button 2'),
-              style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all(Colors.yellow),
-                  backgroundColor: MaterialStateProperty.resolveWith((states) =>
-                      states.any((element) => element == MaterialState.pressed)
-                          ? Colors.purple
-                          : Colors.red), 
-                          overlayColor: MaterialStateProperty.all(Colors.green)),
+            SizedBox(height: 40),
+            ElevatedButton(
+                onPressed: () {
+                  context.read<UserBloc>().add(BirthdayEvent());
+                },
+                child: Text('Birthday')),
+            Container(
+              margin: EdgeInsets.all(15),
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              child: Text('Change Name'),
+              onPressed: () {
+                context.read<UserBloc>().add(ChangeNameEvent(controller.text));
+              },
             ),
           ],
         ),
