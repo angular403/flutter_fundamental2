865a6816075c030f615f98672348327b0018055f
Andrew Napitupulu <angularsprings403@gmail.com>
2023-03-17 09:23:23 +0700

    Firebase Auth Anounymous

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental/pages/pages.dart';
 import 'package:flutter_fundamental/person.dart';
 import 'package:flutter_fundamental/personCard.dart';
 import 'package:flutter_fundamental/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp();
+  // await EasyLocalization.ensureInitialized();
+
   runApp(
     EasyLocalization(
       supportedLocales: [
@@ -30,7 +34,7 @@ class MyApp extends StatelessWidget {
       supportedLocales: context.supportedLocales,
       locale: context.locale,
       debugShowCheckedModeBanner: false,
-      home: LatihanTdd(),
+      home: AnonSignInPage(),
     );
   }
 }
